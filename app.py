81202a7659b8838eee8cd4ce55a1b1ed080d8b12
Santiago-arena-ds <162463941+Santiago-arena-ds@users.noreply.github.com>
2024-03-08 20:04:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -34,6 +34,7 @@ def transcribe_rapido(path):
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
     model_id = "openai/whisper-large-v3"
     model_type = "whisper"
+    config_dict = {"model_type": model_type}
     config = AutoConfig.from_pretrained(config_dict)
     print(model_id)
     model = AutoModelForSpeechSeq2Seq.from_pretrained(
