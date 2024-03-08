a807ea05e56f0d890de51509879003d799a827ea
Santiago-arena-ds <162463941+Santiago-arena-ds@users.noreply.github.com>
2024-03-08 20:09:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -33,9 +33,6 @@ def transcribe_rapido(path):
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
     model_id = "openai/whisper-large-v3"
-    model_type = "whisper"
-    config_dict = {"model_type": model_type}
-    config = AutoConfig.from_pretrained(config_dict)
     print(model_id)
     model = AutoModelForSpeechSeq2Seq.from_pretrained(
         model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
